276e156d01c8b702f2fd8aa87c03952d3ea34bba
Nikolaev <nikolaevkirill237@gmail.com>
2024-12-26 17:22:27 +0300

    classes and methods: implemented

--- src/IncreasingList.java
+++ src/IncreasingList.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class IncreasingList {
+    private List<String> words = new ArrayList<String>();
+    IncreasingList() {}
+    IncreasingList(List<String> arr) {
+        this.words = arr;
+        words.sort((v1, v2) -> Integer.compare(v1.length(), v2.length()));
+    }
+
+    public void add(String val) {
+        this.words.add(val);
+        words.sort((v1, v2) -> Integer.compare(v1.length(), v2.length()));
+    }
+
+    public void showList() {
+        System.out.println(words);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        List<String> d = new ArrayList<String>();
+        d.add("asd");
+        d.add("agdfsd");
+        d.add("asddfgg");
+        d.add("asfd");
+        d.add("asgfgasddd");
+        IncreasingList IL = new IncreasingList(d);
+        IL.showList();
+        IL.add("lfmfoeda");
+        IL.showList();
     }
 }
